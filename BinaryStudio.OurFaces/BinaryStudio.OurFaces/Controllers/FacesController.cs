2aab093be07aeb48b1149237e18eb7528519a959
Nikita Exaurov <exaurov.nm@gmail.com>
2012-10-16 21:43:20 +0300

    Added action and view for adding "face"

---
+++ BinaryStudio.OurFaces/BinaryStudio.OurFaces/Controllers/FacesController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Data;
+using System.Web.Mvc;
+
+namespace BinaryStudio.OurFaces.Controllers
+{
+    public class FacesController : Controller
+    {
+        [HttpGet]
+        public ActionResult AddFace()
+        {
+            return View();
+        }
+
+    }
+
+    public class AddFaceModel
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public DateTime Birthday { get; set; }
+
+        public string ContactPhone { get; set; }
+
+        public string ContactSkype { get; set; }
+
+        public string ContactEmail { get; set; }
+    }
+}
